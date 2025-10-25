3657595dc9aa24c748514a117e2108059a38f3fa
Vladimir Genovich <genovich.v.v@gmail.com>
2025-10-25 23:34:14 +0100

    - update Try to avoid race condition

--- try/src/commonMain/kotlin/com/genovich/components/Try.kt
+++ try/src/commonMain/kotlin/com/genovich/components/Try.kt
@@ -5,24 +5,22 @@ import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.first
 
 class Try<TInput, TOutput, T, TError>(
-    buildAction: (catch: Action<OneOf<T, TError>, T>) -> Action<TInput, TOutput>,
+    val buildAction: (catch: Action<OneOf<T, TError>, T>) -> Action<TInput, TOutput>,
 ) : Action<TInput, OneOf<TOutput, TError>>() {
 
-    // todo possible race condition???
-    private val catchFlow = MutableSharedFlow<TError>()
-    private val func = buildAction(object : Action<OneOf<T, TError>, T>() {
-        override suspend fun invoke(input: OneOf<T, TError>): T {
-            return when (input) {
-                is OneOf.First -> input.first
-                is OneOf.Second -> {
-                    catchFlow.emit(input.second)
-                    awaitCancellation()
+    override suspend fun invoke(input: TInput): OneOf<TOutput, TError> {
+        val catchFlow = MutableSharedFlow<TError>()
+        val func = buildAction(object : Action<OneOf<T, TError>, T>() {
+            override suspend fun invoke(input: OneOf<T, TError>): T {
+                return when (input) {
+                    is OneOf.First -> input.first
+                    is OneOf.Second -> {
+                        catchFlow.emit(input.second)
+                        awaitCancellation()
+                    }
                 }
             }
-        }
-    })
-
-    override suspend fun invoke(input: TInput): OneOf<TOutput, TError> {
+        })
         return parallel(
             { OneOf.First(func(input)) },
             { OneOf.Second(catchFlow.first()) },
